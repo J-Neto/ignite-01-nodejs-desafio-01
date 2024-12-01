41130ec97efc8f8c21568af9760711f5efc77c0a
José Neto <ribeirojoseph44@gmail.com>
2024-12-01 15:11:18 -0300

    :sparkles: feat: Using fs promises

--- src/database.js
+++ src/database.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('./db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
@@ -24,5 +42,13 @@ export class Database {
     }
 
     return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+    }
   }
 }
